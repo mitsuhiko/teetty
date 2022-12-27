b7ac45dec363fcb13ef8eeded12e37cba9906d5c
Armin Ronacher <armin.ronacher@active-4.com>
2022-12-27 19:28:36 +0100

    Added basic test for stdin handling

---
+++ tests/read.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eu
+
+echo 'BEGIN'
+while read -r line; do
+  echo " " $line
+done
+echo 'END'
---
+++ tests/test_stdin.rs
@@ -0,0 +1,30 @@
+use std::fs::{self, File};
+use std::process::Command;
+
+#[test]
+fn test_stdin() {
+    let tempdir = tempfile::tempdir().unwrap();
+    let stdout = tempdir.path().join("stdout");
+    let stderr = tempdir.path().join("stderr");
+    let stdin = tempdir.path().join("stdin");
+    let out = tempdir.path().join("out");
+    fs::write(&stdin, "line1\nline2\n").unwrap();
+    let status = Command::new(env!("CARGO_BIN_EXE_teetty"))
+        .arg("--out")
+        .arg(&out)
+        .arg("--")
+        .arg("tests/read.sh")
+        .stdout(File::create(&stdout).unwrap())
+        .stderr(File::create(&stderr).unwrap())
+        .stdin(File::open(&stdin).unwrap())
+        .status()
+        .unwrap();
+    let stdout = fs::read_to_string(&stdout).unwrap();
+    let stderr = fs::read_to_string(&stderr).unwrap();
+    let out = fs::read_to_string(&out).unwrap();
+
+    assert_eq!(status.code(), Some(0));
+    assert_eq!(stderr, "");
+    assert_eq!(out, "BEGIN\n  line1\n  line2\nEND\n");
+    assert_eq!(stdout, "BEGIN\n  line1\n  line2\nEND\n");
+}
