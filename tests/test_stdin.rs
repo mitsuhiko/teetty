34e4497e9331344c94a41a6ce0a5f675ea1868f6
Armin Ronacher <armin.ronacher@active-4.com>
2022-12-27 19:29:40 +0100

    Added test on output processing

--- tests/test_stdin.rs
+++ tests/test_stdin.rs
@@ -28,3 +28,32 @@ fn test_stdin() {
     assert_eq!(out, "BEGIN\n  line1\n  line2\nEND\n");
     assert_eq!(stdout, "BEGIN\n  line1\n  line2\nEND\n");
 }
+
+#[test]
+fn test_stdin_output_processing() {
+    let tempdir = tempfile::tempdir().unwrap();
+    let stdout = tempdir.path().join("stdout");
+    let stderr = tempdir.path().join("stderr");
+    let stdin = tempdir.path().join("stdin");
+    let out = tempdir.path().join("out");
+    fs::write(&stdin, "line1\nline2\n").unwrap();
+    let status = Command::new(env!("CARGO_BIN_EXE_teetty"))
+        .arg("--out")
+        .arg(&out)
+        .arg("--output-processing")
+        .arg("--")
+        .arg("tests/read.sh")
+        .stdout(File::create(&stdout).unwrap())
+        .stderr(File::create(&stderr).unwrap())
+        .stdin(File::open(&stdin).unwrap())
+        .status()
+        .unwrap();
+    let stdout = fs::read_to_string(&stdout).unwrap();
+    let stderr = fs::read_to_string(&stderr).unwrap();
+    let out = fs::read_to_string(&out).unwrap();
+
+    assert_eq!(status.code(), Some(0));
+    assert_eq!(stderr, "");
+    assert_eq!(out, "BEGIN\r\n  line1\r\n  line2\r\nEND\r\n");
+    assert_eq!(stdout, "BEGIN\r\n  line1\r\n  line2\r\nEND\r\n");
+}
