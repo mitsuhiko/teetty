41ff2d8617ab6543f05c63d2027902f16683d9d7
Armin Ronacher <armin.ronacher@active-4.com>
2022-12-30 11:26:49 +0100

    Fix publish command

--- scripts/publish-all.sh
+++ scripts/publish-all.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 
-cargo publish tty-spawn
-cargo publish teetty
+cargo publish -p tty-spawn
+cargo publish -p teetty
